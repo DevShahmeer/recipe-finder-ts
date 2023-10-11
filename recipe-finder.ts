a8ab574514f96a42ee1b778da991ea1f09a84829
shahmeer <theshahmeerfsd@gmail.com>
2023-10-11 20:20:40 +0500

    Added Recipe interface and mock data

--- recipe-finder.ts
+++ recipe-finder.ts
@@ -14,25 +14,5 @@ interface Recipe {
     // Add more recipes...
   ];
   
-  // Function to fetch recipes (mocked for simplicity)
-  function fetchRecipes(): Recipe[] {
-    return recipes;
-  }
-  
-  // Function to display a recipe
-  function displayRecipe(recipe: Recipe) {
-    console.log('Recipe Title:', recipe.title);
-    console.log('Ingredients:', recipe.ingredients.join(', '));
-    console.log('Instructions:', recipe.instructions);
-    console.log('---------------------');
-  }
-  
-  // Main function to run the application
-  function main() {
-    const recipes = fetchRecipes(); // Fetch recipes
-    recipes.forEach(displayRecipe); // Display each recipe
-  }
-  
-  main(); // Run the application
 
   
