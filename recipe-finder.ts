afefb9438658042b8b6ad423d11dd746e1684e0d
shahmeer <theshahmeerfsd@gmail.com>
2023-10-11 20:19:06 +0500

    first commit

---
+++ recipe-finder.ts
@@ -0,0 +1,38 @@
+interface Recipe {
+    title: string;
+    ingredients: string[];
+    instructions: string;
+  }
+  
+  // Mock recipes data
+  const recipes: Recipe[] = [
+    {
+      title: 'Pasta Carbonara',
+      ingredients: ['pasta', 'eggs', 'bacon', 'parmesan', 'garlic'],
+      instructions: '...'
+    },
+    // Add more recipes...
+  ];
+  
+  // Function to fetch recipes (mocked for simplicity)
+  function fetchRecipes(): Recipe[] {
+    return recipes;
+  }
+  
+  // Function to display a recipe
+  function displayRecipe(recipe: Recipe) {
+    console.log('Recipe Title:', recipe.title);
+    console.log('Ingredients:', recipe.ingredients.join(', '));
+    console.log('Instructions:', recipe.instructions);
+    console.log('---------------------');
+  }
+  
+  // Main function to run the application
+  function main() {
+    const recipes = fetchRecipes(); // Fetch recipes
+    recipes.forEach(displayRecipe); // Display each recipe
+  }
+  
+  main(); // Run the application
+
+  
